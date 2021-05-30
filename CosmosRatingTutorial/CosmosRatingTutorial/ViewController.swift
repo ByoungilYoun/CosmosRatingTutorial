46fa90b5d52e492429a950b09ff443213477a84b
YounByoungil <jimmy7525@naver.com>
2021-05-31 01:05:12 +0900

    Setup Project

---
+++ CosmosRatingTutorial/CosmosRatingTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CosmosRatingTutorial
+//
+//  Created by 윤병일 on 2021/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+
+
+}
+
